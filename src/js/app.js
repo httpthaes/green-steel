45744366e93efc9540ce8c6772311dc1fa4dd98d
Thais <thaiserica1304@gmail.com>
2026-05-25 19:32:02 -0300

    remove timeline and add dados

--- src/js/app.js
+++ src/js/app.js
@@ -42,68 +42,37 @@ btnLang.forEach(() => {
     })
 })
 
-// Conteúdo timeline
+// dados
 
-const eventos = [
+const container = document.querySelector('.impacto__grid')
+
+const dados = [
     {
-        ano: "1850",
-        titulo: "Início da industrialização",
-        descricao: "CO₂ em 280 ppm. Início da siderurgia moderna com coque de carvão."
+        title: "8%",
+        descricao: "das emissões globais vêm da indústria do aço"
     },
     {
-        ano: "1950",
-        titulo: "Boom do aço pós-guerra",
-        descricao: "As emissões industriais globais dobram em relação a 1900. O aço vira símbolo do progresso do pós-guerra."
+        title: "+52%",
+        descricao: "aumento do CO₂ desde a era industrial"
     },
     {
-        ano: "1988",
-        titulo: "IPCC criado pela ONU",
-        descricao: "Criação do IPCC pela ONU. Primeiro consenso científico formal sobre o aquecimento causado por humanos."
+        title: "1,9 bilhão",
+        descricao: "de toneladas de aço produzidas por ano"
     },
     {
-        ano: "2000",
-        titulo: "Alerta climático global",
-        descricao: "CO₂ chega a 370 ppm. Protocolo de Kyoto já em vigor, mas grandes industriais fora do acordo."
-    },
-    {
-        ano: "2015",
-        titulo: "Acordo de Paris",
-        descricao: "Acordo de Paris. Meta de limitar aquecimento a 1,5°C. A indústria do aço responde por 7–9% das emissões globais."
-    },
-    {
-        ano: "2023/2024",
-        titulo: "Nível mais alto em 3 milhões de anos",
-        descricao: "424 ppm. O planeta já aqueceu 1,2°C desde a era pré-industrial. 12 dos últimos 13 anos foram os mais quentes da história registrada."
+        title: "até 68%",
+        descricao: "menos emissões com aço XCarb®"
     }
 ]
 
-function renderTimeline() {
-    const timeline = document.querySelector('#timeline');
+dados.forEach((dado) => {
+    container.innerHTML += `
+        <div class="impacto__card">
+            <span class="impacto__dado">${dado.title}</span>
+            <p>${dado.descricao}</p>
+        </div>
+        `
+})
 
-    eventos.forEach((evento, index) => {
-        const item = document.createElement('li');
 
-        item.innerHTML = `
-            <div class="timeline__card">
-
-                <span class="timeline__ano">
-                    ${evento.ano}
-                </span>
-
-                <h3 class="timeline__titulo">
-                    ${evento.titulo}
-                </h3>
-
-                <p class="timeline__descricao">
-                    ${evento.descricao}
-                </p>
-
-            </div>
-        `;
-
-        timeline.appendChild(item);
-    })
-}
-
-renderTimeline()
-
+// produtos xcarb
